0dd56e2b11b33991ad0cb81ec3e19945f828b9db
Toralf Richter <73883286+snowgames95@users.noreply.github.com>
2021-09-12 13:13:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+clear
+
+############################################
+# Installation des Winlaufen Sprecher-PC´s #
+############################################
+
+sudo apt update -y && sudo apt upgrade -y
+mkdir ~/snowgames
+sudo mv -v ~/snowgamessources/SprecherPCtoLinux/javaw.ico /usr/share/pixmaps/sprecherpc.ico
+sudo mv -v ~/snowgamessources/SprecherPCtoLinux/SprecherPC.jar ~/snowgames/SprecherPC.jar
+
+cat <<EOF ~/.local/share/applications/SprecherPC.desktop
+[Desktop Entry]
+Name=SprecherPC
+Comment=Startet den Winlaufen Sprecher-PC
+Icon=/usr/share/pixmaps/sprecherpc.ico
+Exec=java -jar -Dfile.encoding=ISO-8859-1 ~/snowgames/SprecherPC.jar
+Type=Application
+Terminal=false
+Categories=None;
+Name[de_DE]=SprecherPC
+EOF
+
+sudo chmod +x ~/.local/share/applications/SprecherPC.desktop
